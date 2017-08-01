f1f914760d0ca6428db791f5c41728be11694719
RickyAmparo <rickyamparo@gmail.com>
2017-08-01 10:29:07 -0600

    add test for what_can_i_make method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -79,7 +79,19 @@ class PantryTest < Minitest::Test
     r3.add_ingredient("Raw nuts", 10)
     r3.add_ingredient("Salt", 10)
 
+    @pantry.add_to_cookbook(r1)
+    @pantry.add_to_cookbook(r2)
+    @pantry.add_to_cookbook(r3)
 
+    @pantry.restock("Cheese", 10)
+    @pantry.restock("Flour", 20)
+    @pantry.restock("Brine", 40)
+    @pantry.restock("Raw nuts", 20)
+    @pantry.restock("Salt", 20)
+
+    expected ["Brine Shot", "Peanuts"]
+
+    assert_equal expected, @pantry.what_can_i_make
   end
 
 
